9003348c0d815204f919f33f7c81303672ae2a9c
WeAsAn <weasan@yandex.ru>
2022-06-24 14:08:59 +0300

    version 0.0.1

--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,7 @@
 
 const Hero = require('./game-models/Hero');
 const Enemy = require('./game-models/Enemy');
-// const Boomerang = require('./game-models/Boomerang');
+const Boomerang = require('./game-models/Boomerang');
 const View = require('./View');
 
 // Основной класс игры.
@@ -13,7 +13,8 @@ const View = require('./View');
 class Game {
   constructor({ trackLength }) {
     this.trackLength = trackLength;
-    this.hero = new Hero(); // Герою можно аргументом передать бумеранг.
+    this.hero = new Hero({ position: 1 }); // Герою можно аргументом передать бумеранг.
+    this.boomerang = new Boomerang();
     this.enemy = new Enemy();
     this.view = new View();
     this.track = [];
@@ -23,13 +24,17 @@ class Game {
   regenerateTrack() {
     // Сборка всего необходимого (герой, враг(и), оружие)
     // в единую структуру данных
-    this.track = (new Array(this.trackLength)).fill(' ');
+    this.track = new Array(this.trackLength).fill(' ');
     this.track[this.hero.position] = this.hero.skin;
+    this.track[this.enemy.position] = this.enemy.skin;
   }
 
   check() {
     if (this.hero.position === this.enemy.position) {
       this.hero.die();
+    }
+    if (this.enemy.position === this.boomerang.position && this.boomerang.thrown) {
+      this.enemy.die();
     }
   }
 
@@ -37,10 +42,14 @@ class Game {
     setInterval(() => {
       // Let's play!
       this.check();
+      this.enemy.moveLeft();
       this.regenerateTrack();
       this.view.render(this.track);
-    });
+    }, 200);
   }
 }
+
+const game = new Game({ trackLength: 32 });
+game.play();
 
 module.exports = Game;
